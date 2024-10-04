cf81b3e8a329e069f5529390f4ac0f5807931b28
Adil Mohak <adilmohak123@gmail.com>
2024-10-05 01:17:28 +0300

    Follow pylint rule for quiz app

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,23 +1,23 @@
 from django.urls import path
-from .views import *
+from . import views
 
 urlpatterns = [
-    path("<slug>/quizzes/", quiz_list, name="quiz_index"),
-    path("progress/", view=QuizUserProgressView.as_view(), name="quiz_progress"),
+    path("<slug>/quizzes/", views.quiz_list, name="quiz_index"),
+    path("progress/", view=views.QuizUserProgressView.as_view(), name="quiz_progress"),
     # path('marking/<int:pk>/', view=QuizMarkingList.as_view(), name='quiz_marking'),
-    path("marking_list/", view=QuizMarkingList.as_view(), name="quiz_marking"),
+    path("marking_list/", view=views.QuizMarkingList.as_view(), name="quiz_marking"),
     path(
         "marking/<int:pk>/",
-        view=QuizMarkingDetail.as_view(),
+        view=views.QuizMarkingDetail.as_view(),
         name="quiz_marking_detail",
     ),
-    path("<int:pk>/<slug>/take/", view=QuizTake.as_view(), name="quiz_take"),
-    path("<slug>/quiz_add/", QuizCreateView.as_view(), name="quiz_create"),
-    path("<slug>/<int:pk>/add/", QuizUpdateView.as_view(), name="quiz_update"),
-    path("<slug>/<int:pk>/delete/", quiz_delete, name="quiz_delete"),
+    path("<int:pk>/<slug>/take/", view=views.QuizTake.as_view(), name="quiz_take"),
+    path("<slug>/quiz_add/", views.QuizCreateView.as_view(), name="quiz_create"),
+    path("<slug>/<int:pk>/add/", views.QuizUpdateView.as_view(), name="quiz_update"),
+    path("<slug>/<int:pk>/delete/", views.quiz_delete, name="quiz_delete"),
     path(
         "mc-question/add/<slug>/<int:quiz_id>/",
-        MCQuestionCreate.as_view(),
+        views.MCQuestionCreate.as_view(),
         name="mc_create",
     ),
     # path('mc-question/add/<int:pk>/<quiz_pk>/', MCQuestionCreate.as_view(), name='mc_create'),
