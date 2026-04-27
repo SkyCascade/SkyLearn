f9826cb118f87a9e4acece0de51ff58b57309f57
Julius Boakye <julius.boakye@paicore.tech>
2026-04-27 11:08:44 +0000

    fix: update ALLOWED_HOSTS to include wifflms.pythonanywhere.com

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=True, cast=bool)
 
-ALLOWED_HOSTS = ["127.0.0.1", "adilmohak1.pythonanywhere.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "wifflms.pythonanywhere.com"]
 
 # change the default user models to our custom model
 AUTH_USER_MODEL = "accounts.User"
